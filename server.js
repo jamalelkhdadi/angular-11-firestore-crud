bbcb32c3ac2bfe5a1673a145450ea1b71cf16268
jamalelkhdadi <jamal.elkhdadi@outlook.com>
2021-01-10 15:56:55 +0100

    56

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const http = require('http');
+// const http = require('http');
 
 const app = express();
 
+app.use(express.static('dist'));
 
-app.use(express.static('./dist/'));
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: '/dist/' }),
+  res.sendFile('index.html', {
+    root: '/dist/'
+  }),
 );
 
 app.listen(process.env.PORT || 8080);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB7M6dKcU3dGqUmFEBH_c5_ksoUwY_Gths",
+    authDomain: "ng-fire-express-node.firebaseapp.com",
+    databaseURL: "https://ng-fire-express-node.firebaseio.com",
+    projectId: "ng-fire-express-node",
+    storageBucket: "ng-fire-express-node.appspot.com",
+    messagingSenderId: "838153226042",
+    appId: "1:838153226042:web:6488ca6cc172f372bab203",
+    measurementId: "G-19KNWHKJN6"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
